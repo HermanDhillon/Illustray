f8614d514572a3987023ef554b8fff4922384d42
Omi <digicoder@protonmail.com>
2023-03-18 18:32:38 -0400

    Setup linter (eslint) and formatter (prettier) for Daazzll project.

--- server/server.js
+++ server/server.js
@@ -2,38 +2,40 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
-const pgPool= require('./config/database');
+const pgPool = require('./config/database');
 const authRouter = require('./routes//auth');
 const dotenv = require('dotenv');
 
 if (process.env.NODE_ENV === 'production') {
-    dotenv.config('./.env.prod');
-  }else{
-    dotenv.config('./.env.dev');
-  }
+  dotenv.config('./.env.prod');
+} else {
+  dotenv.config('./.env.dev');
+}
 
-let {PORT} = process.env;
+let { PORT } = process.env;
 
 // Body Parsing
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Session store and session config
-app.use(session({
+app.use(
+  session({
     store: new pgSession({
-      pool : pgPool,                
-      tableName : 'user_sessions',
-      createTableIfMissing: true,
+      pool: pgPool,
+      tableName: 'user_sessions',
+      createTableIfMissing: true
       // Insert connect-pg-simple options here
     }),
     secret: process.env.SESSION_COOKIE_SECRET,
     resave: false,
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
     // Insert express-session options here
-  }));
+  })
+);
 
-app.use("/api/auth", authRouter)
+app.use('/api/auth', authRouter);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
