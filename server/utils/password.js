c2a5c785dfa2fcac4c8d9b019663cafd81938096
hermandhillon <dhillonherman1991@gmail.com>
2023-03-21 20:33:54 -0700

    add auth logic and install validator library

--- server/utils/password.js
+++ server/utils/password.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 function hashifier(password) {
     let salt = crypto.randomBytes(32).toString('hex');
-    let hashedPass = crypto.pbkdf2Sync(password, salt, 15000, 64, 'sha512').toString('hex');
+    let hashedPass = crypto.pbkdf2Sync(password, salt, 150000, 64, 'sha512').toString('hex');
     return {
         salt: salt,
         hash: hashedPass,
@@ -10,7 +10,7 @@ function hashifier(password) {
 }
 
 function validatePass(password, hash, salt) {
-    let verifyHash = crypto.pbkdf2Sync(password, salt, 15000, 64, 'sha512').toString('hex');
+    let verifyHash = crypto.pbkdf2Sync(password, salt, 150000, 64, 'sha512').toString('hex');
     return hash === verifyHash;
 }
 
