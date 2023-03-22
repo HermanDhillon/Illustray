46af2929fc7808c54a0e85d2e9105cab4543a58e
hermandhillon <dhillonherman1991@gmail.com>
2023-03-22 00:55:14 -0700

    modify login route handler

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,7 @@ let {hashifier} = require('../utils/password');
 const validator = require('validator');
 
 module.exports = {
+
     postLogin: (req, res, next) => {
         const validationErrors = [];
         if (!validator.isEmail(req.body.email))
@@ -36,5 +37,56 @@ module.exports = {
         })(req, res, next);
     },
 
-    postSignup: 
+    // postSignup: (req, res, next) => {
+    //     const validationErrors = [];
+    //     if (!validator.isEmail(req.body.email))
+    //       validationErrors.push({ msg: "Please enter a valid email address." });
+    //     if (!validator.isLength(req.body.password, { min: 8 }))
+    //       validationErrors.push({
+    //         msg: "Password must be at least 8 characters long",
+    //       });
+    //     if (req.body.password !== req.body.confirmPassword)
+    //       validationErrors.push({ msg: "Passwords do not match" });
+      
+    //     if (validationErrors.length) {
+    //       req.flash("errors", validationErrors);
+    //       return res.redirect("../signup");
+    //     }
+    //     req.body.email = validator.normalizeEmail(req.body.email, {
+    //       gmail_remove_dots: false,
+    //     });
+      
+    //     const user = new User({
+    //       userName: req.body.username,
+    //       email: req.body.email,
+    //       password: req.body.password,
+    //     });
+      
+    //     User.findOne(
+    //       { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+    //       (err, existingUser) => {
+    //         if (err) {
+    //           return next(err);
+    //         }
+    //         if (existingUser) {
+    //           req.flash("errors", {
+    //             msg: "Account with that email address or username already exists.",
+    //           });
+    //           return res.redirect("../signup");
+    //         }
+    //         user.save((err) => {
+    //           if (err) {
+    //             return next(err);
+    //           }
+    //           req.logIn(user, (err) => {
+    //             if (err) {
+    //               return next(err);
+    //             }
+    //             res.redirect("/profile");
+    //           });
+    //         });
+    //       }
+    //     );
+    //   },
+      
 }
