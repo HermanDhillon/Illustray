11d61f5e11f962a484636df9e290fc0fac21a380
Herman Dhillon <105324058+HermanDhillon@users.noreply.github.com>
2023-05-16 15:30:36 -0700

    Containerize (#16)
    * Create docker file for react vite frontend UI
    
    * create dockerfile for api backend
    
    * create docker compose file
    
    * modify vite proxy to point to new backend api on docker network
    
    * in compose file, changed postgres db volume to named volume to allow docker to manage its location
    
    * modify docker-compose.yaml to accept a .env file in the api container, rather than explicly declaring each environment variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:5000/",
+      "/api": "http://api:3000",
     },
   },
   plugins: [react()],
