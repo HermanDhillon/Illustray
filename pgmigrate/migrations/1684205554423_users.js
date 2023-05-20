4bd5f38ad7b70e844ea42094df5c2e6dbde428b9
hermandhillon <dhillonherman1991@gmail.com>
2023-05-19 21:21:30 -0700

    create migrations for several tables

--- pgmigrate/migrations/1684205554423_users.js
+++ pgmigrate/migrations/1684205554423_users.js
@@ -3,17 +3,16 @@
 exports.shorthands = undefined;
 
 exports.up = pgm => {
-    pgm.createTable('users', {
-        id: 'id',
-        username: { type: 'varchar(32)', notNull: true, unique: true },
-        email: { type: 'varchar(100)', notNull: true, unique: true },
-        hash: {type:'varchar(64)', notNull: true},
-        createdAt: {type: 'timestamp', notNull: true, default: pgm.func('current_timestamp')},
-        updatedAt: {type: 'timestamp', notNull: true, default: pgm.func('current_timestamp')},
-        profileImage: {type: 'string', notNull: true, default: 'https://cdn.dribbble.com/users/6142/screenshots/5679189/media/052967c305a8f96a4b40b79ce5e61b0d.png'},
-        bio: {type: 'varchar(400)', default: 'This user has not written a bio yet'}
-        },
-      )
+      pgm.sql(`CREATE TABLE users (
+        id serial PRIMARY KEY,
+        username VARCHAR(32) UNIQUE NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        hash VARCHAR(64) NOT NULL,
+        createdAT TIMESTAMP NOT NULL,
+        lastLogin TIMESTAMP,
+        profileImage VARCHAR(1000) DEFAULT 'https://cdn.dribbble.com/users/6142/screenshots/5679189/media/052967c305a8f96a4b40b79ce5e61b0d.png',
+        bio VARCHAR(400) DEFAULT 'This user has no bio yet'
+      );
+      `)
 };
 
-exports.down = pgm => {};
