264ec596493f5710fefba006c55b4e669f2785cd
hermandhillon <dhillonherman1991@gmail.com>
2023-06-04 11:30:09 -0700

    add react router and seperate pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import Login from './components/Login'
-import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
 import Signup from './components/Signup'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Login />
-      <Signup />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   )
 }
