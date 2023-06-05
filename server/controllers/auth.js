45253f65ea19b14967df5d3186346b7225152595
hermandhillon <dhillonherman1991@gmail.com>
2023-06-05 02:01:38 -0700

    i went overboard on changes :(

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,38 +5,31 @@ const { hashifier } = require('../utils/password');
 
 module.exports = {
   postLogin: (req, res, next) => {
-    const validationErrors = [];
-
-    if (!validator.isEmail(req.body.email)) {
-      validationErrors.push({ msg: 'Please enter a valid email address.' });
+    // if (!validator.isEmail(req.body.email)) {
+    //   validationErrors.push({ msg: 'Please enter a valid email address.' });
+    // }
+    if (
+      validator.isEmpty(req.body.password) ||
+      validator.isEmpty(req.body.username)
+    ) {
+      return res.json({ error: 'Fields cannot be blank.' });
     }
 
-    if (validator.isEmpty(req.body.password)) {
-      validationErrors.push({ msg: 'Password cannot be blank.' });
-    }
-
-    if (validationErrors.length) {
-      req.flash('errors', validationErrors);
-      return res.redirect('/login');
-    }
-
-    req.body.email = validator.normalizeEmail(req.body.email, {
-      gmail_remove_dots: true,
-    });
+    // req.body.email = validator.normalizeEmail(req.body.email, {
+    //   gmail_remove_dots: true,
+    // });
 
     passport.authenticate('local', (err, user, info) => {
       if (err) {
         return next(err);
       }
       if (!user) {
-        req.flash('errors', info);
-        return res.redirect('/login');
+        return res.json({ error: 'Incorrect username or password.' });
       }
       req.logIn(user, (error) => {
         if (error) {
           return next(error);
         }
-        req.flash('success', { msg: 'Success! You are logged in.' });
         // res.redirect(req.session.returnTo || '/');
         res.cookie('userid', user.id, { maxAge: 24 * 60 * 60 * 1000 }); // Expires in 01 days
         res.json({ login: 'successful' });
@@ -46,30 +39,32 @@ module.exports = {
 
   postSignUp: async (req, res, next) => {
     const validationErrors = [];
-    const { username, password } = req.body;
+    const { username, password, conPassword } = req.body;
     let { email } = req.body;
 
     if (!validator.isEmail(email)) {
-      validationErrors.push({ msg: 'Please enter a valid email address.' });
+      validationErrors.push('Please enter a valid email address.');
     }
     if (!validator.isStrongPassword(password)) {
-      validationErrors.push({
-        msg: 'Password must be atleast 8 characters long, Contain atleast 1 lowercase letter, 1 uppercase letter, 1 number and 1 symbol.',
-      });
+      validationErrors.push(
+        'Password must be atleast 8 characters long, Contain atleast 1 lowercase letter, 1 uppercase letter, 1 number and 1 symbol.'
+      );
+    }
+    if (password !== conPassword) {
+      validationErrors.push('Passwords do not match');
     }
     if (!validator.isAlphanumeric(username, 'en-US', { ignore: '_' })) {
-      validationErrors.push({
-        msg: 'Username can only contain letters, numbers and underscores.',
-      });
+      validationErrors.push(
+        'Username can only contain letters, numbers and underscores.'
+      );
     }
     if (!validator.isLength(username, { min: 4, max: 30 })) {
-      validationErrors.push({
-        msg: 'Username must be between 4 and 30 characters long.',
-      });
+      validationErrors.push(
+        'Username must be between 4 and 30 characters long.'
+      );
     }
     if (validationErrors.length) {
-      req.flash('errors', validationErrors);
-      return res.redirect('/signup');
+      return res.json({ error: validationErrors });
     }
 
     email = validator.normalizeEmail(email, { gmail_remove_dots: true });
@@ -78,9 +73,8 @@ module.exports = {
     const userLookup = await User.findByEmailOrUsername(email, username);
 
     if (userLookup) {
-      req.flash('errors', { msg: 'Username or email already in use.' });
       console.log('email already exists');
-      return res.redirect('/signup');
+      return res.json({ error: ['Username or email already in use.'] });
     }
 
     const user = await User.create(username, email, hash);
@@ -88,7 +82,8 @@ module.exports = {
       if (err) {
         return next(err);
       }
-      res.redirect('/');
+      res.cookie('userid', user.id, { maxAge: 24 * 60 * 60 * 1000 }); // Expires in 01 days
+      res.json({ signup: 'successful' });
     });
   },
 };
