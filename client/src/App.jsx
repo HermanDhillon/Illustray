891192d254719b1f7d8e2ec6be01eed9d9cc3e83
hermandhillon <dhillonherman1991@gmail.com>
2023-06-07 13:51:07 -0700

    add profile page html and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Signup from './components/Signup'
 import Settings from './components/Settings'
+import Profile from './components/Profile'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   )
