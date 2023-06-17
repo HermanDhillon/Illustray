d0cee4551296200874b91c49fe12b0dafa6bb222
hermandhillon <dhillonherman1991@gmail.com>
2023-06-17 01:39:25 -0700

    ui adjusments for image modal

--- client/src/components/GalleryHome.jsx
+++ client/src/components/GalleryHome.jsx
@@ -38,22 +38,22 @@ export default function GalleryHome(props) {
 
       <dialog
         id="image_modal"
-        className="drop-shadow-2xl shadow-2xl rounded-xl w-[100rem] overflow-hidden "
+        className="drop-shadow-2xl shadow-2xl rounded-xl w-[100rem] max-h-[80vh] overflow-hidden "
       >
-        <div className="flex flex-col justify-between mx-auto md:flex-row">
+        <div className="flex flex-col justify-between my-[2.5vh] mx-auto md:flex-row">
           <button
             onClick={() => window.image_modal.close()}
             className="btn btn-sm btn-circle absolute right-2 top-2 border-none"
           >
             ✘
           </button>
-          <div className="h-[50%] md:w-[60%] rounded-xl bg-slate-500 bg-opacity-10">
+          <div className=" flex flex-col align-middle rounded-xl bg-slate-500 bg-opacity-10 md:w-[65%] md:max-h-[71vh]">
             <img
               src={source}
-              className="rounded-xl max-h-[40rem] md:max-h-[40rem] mx-auto"
+              className="rounded-xl max-h-[45vh] md:max-h-[71vh] m-auto p-auto"
             ></img>
           </div>
-          <div className="min-h-[100px] mt-5 rounded-xl border border-#c4c9d28b md:ml-5 md:mt-0 md:w-[40%] ">
+          <div className="mt-5 max-h-[40vh] rounded-xl border border-#c4c9d28b md:ml-5 md:mt-0 md:w-[35%] md:max-h-[71vh] ">
             <h3>Comments</h3>
           </div>
         </div>
