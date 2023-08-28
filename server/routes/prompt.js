6a059c50e04f2ae55920b59470e0c7f6112aed00
hermandhillon <dhillonherman1991@gmail.com>
2023-08-27 21:51:10 -0700

    create middleware directory and ensureAuth middleware, added ensureAuth to prompt post route

--- server/routes/prompt.js
+++ server/routes/prompt.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const { ensureAuth } = require('../middleware/auth');
 const promptController = require('../controllers/prompt');
 
-router.post('/', promptController.create);
+router.post('/', ensureAuth, promptController.create);
 router.get('/:promptId', promptController.getPrompt);
 
 module.exports = router;
