0a1bc1eac6ab18602594c2c26e0757c473ebf958
HermanDhillon <dhillonherman1991@gmail.com>
2023-09-30 02:11:47 -0700

    isolate client server

--- pgmigrate/migrations/1694029743810_posts.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable camelcase */
-
-exports.shorthands = undefined;
-
-exports.up = (pgm) => {
-  pgm.sql(`ALTER TABLE posts
-  ADD COLUMN prompt_id INT NOT NULL 
-  REFERENCES prompts (id)`);
-};
